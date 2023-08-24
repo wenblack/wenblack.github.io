ce61198093a21ff82f94ebb5dbfa6dc1d85674ae
Wen Barbosa <wenblack012@gmail.com>
2023-08-24 13:08:01 -0300

    ➕ Created next config file ➕

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'github.com',
-            },
-        ],
-    },
-    ...nextConfig
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'http',
+              hostname: 'github.com',
+          },
+      ],
+  },
+  ...nextConfig
 }
 
