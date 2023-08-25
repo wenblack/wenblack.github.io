d793f3f04bd2fdda2cfa6a83d824456346bea9f3
Wen Barbosa <wenblack012@gmail.com>
2023-08-25 09:45:29 -0300

    Created aria label for button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ export function  Header () {
         <a  href="#" className="text-white font-semibold text-lg">WEN BARBOSA</a>
 
         {/* Mobile Menu Button */}
-        <button className="mobile-menu-button md:hidden" onClick={toggleMenu}>
+        <button aria-label='menu button' className="mobile-menu-button md:hidden" onClick={toggleMenu}>
           <svg className="h-6 w-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
