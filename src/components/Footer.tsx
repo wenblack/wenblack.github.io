cd4501f53196e02ede1c036b06c1493e2a172024
Wen Barbosa <wenblack012@gmail.com>
2023-08-28 09:59:29 -0300

    Update footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer  ()  {
   return (
     <footer className="py-4 mt-8 border-t border-zinc-800">
       <div className="container mx-auto px-4">
-        <p className="text-center text-gray-500">© 2023 WenBarbosa. All rights reserved.</p>
+        <p className="text-center text-gray-300">© 2023 WenBarbosa. All rights reserved.</p>
       </div>
     </footer>
   );
